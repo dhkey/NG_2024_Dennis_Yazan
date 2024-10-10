26f6f85fd9bdb2e8e2dffd9e9375139778a22733
dhkey <dhkey14@icloud.com>
2024-10-10 14:36:41 +0300

    fixed task6, switch was deleted, bad option

--- Lesson_1/Task_6/main.cpp
+++ Lesson_1/Task_6/main.cpp
@@ -1,90 +1,76 @@
 #include <iostream>
+#include <unistd.h>
 #include <cmath>
-
-#include <unistd.h> // containts delay function for unix systems
-// #include <windows.h> // contains delay function for windows
-
-#include <cstdlib>
 using namespace std;
 
-const short int rows = 5;
-const short int columns = 5;
-
-void cleanScreen(){
-    cout << "\033[2J\033[1;1H";
-    return;
-}
-
-void printMatrix(char matrix[rows][columns]){
-    for (int i=0; i<rows; i++){
-        for (int j=0; j<columns; j++){
-            cout << matrix[i][j] << " ";
-        }
-        cout << "\n";
-    }
-    return;
-}
-
-int main()
-{
-    int elemAftercenter = floor(columns/2);
-    int centerColumns = ceil(columns/2);
-    int centerRows = ceil(rows/2);
-    char matrix[rows][columns];
-
-
+void printMatrix(char** matrix, int rows, int columns) { // вивод матриці
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
-            if (j == centerColumns) {
-                matrix[i][j] = '*';
-            } else {
-                matrix[i][j] = '-';
+            cout << matrix[i][j];
+        }
+        cout << endl;
+    }
+}
+
+void clearMatrix(char** matrix, int rows, int columns) { // очистка матриці
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            matrix[i][j] = ' ';
+        }
+    }
+}
+
+int main() {
+    int rows = 10;
+    int columns = 12;
+    int sizeOfTreangle = 5;
+
+    char** matrix = new char*[rows];
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new char[columns];
+    }// дінамічно забиваєм
+
+    int angle = 0; // угл наклона
+
+    int centerX = columns / 2; // центр оборота відносно якого будім спінить
+    int centerY = rows / 2; // центр оборота відносно якого будім спінить
+
+    while (true) {
+        cout << "\033[2J\033[1;1H"; //чістім
+
+        clearMatrix(matrix, rows, columns);
+
+        for (int x = 0; x < sizeOfTreangle; x++) {
+            int starsCount = x + 1;
+
+            for (int y = 0; y < starsCount; y++) {
+
+                double radians = angle * M_PI / 180;
+                double cosVariable =  cos(radians);
+                double sinVariable = sin(radians);
+
+                int nX = round(centerX + (x * cosVariable - y * sinVariable)); // наскока точка улетіла по іксу
+                int nY = round(centerY + (x * sinVariable + y * cosVariable)); // наскока точка улетіла по ігріку
+
+                // cout << nY << endl;
+                // cout << nX << endl;
+
+                if (nY >= 0 && nY < rows && nX >= 0 && nX < columns) { // чи не улетіли ми за рамки
+                    matrix[nY][nX] = '*';
+                }
             }
         }
-    }
 
-    int step = 0;
+        printMatrix(matrix, rows, columns); // аутпут
 
-    while (1){
-
-        if ( step == 4 ) {
-            step = 0;
+        if (angle % 90 == 0) {
+            usleep(1000000); //для кращого віз сприйняття лучше трішки застопить тріугольнік в ровних позиціях
+        } else {
+            usleep(200000/2);
         }
 
-        switch(step){
-            case 0:
-                for (int i=0; i<rows; i++){
-                    matrix[i][centerColumns] = '-';
-                    matrix[i][centerColumns+elemAftercenter-i] = '*';
-                }
-                break;
-            case 1:
-                for (int i=0; i<columns; i++){
-                    matrix[i][centerColumns+elemAftercenter-i] = '-';
-                    matrix[centerRows][i] ='*';
-                }
-                break;
-            case 2:
-                for (int i=0; i<rows; i++){
-                    matrix[centerRows][i] = '-';
-                    matrix[i][i] = '*';
-                }
-                break;
-            case 3:
-                for (int i=0; i<columns; i++){
-                    matrix[i][i] = '-';
-                    matrix[i][centerColumns] ='*';
-                }
-                break;
-        }
-
-        step = step + 1;
-
-
-
-        printMatrix(matrix);
-
-        cleanScreen();
-        usleep(1000000/2);
+        angle += 10; // міняємо сам кут аби плавно було
+        if (angle >= 360) angle = 0; //шоб змінна не улітучілась в агромні значення угла
     }
+
 }
