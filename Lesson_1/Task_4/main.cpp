60f18e4041a3b3c105a5ef21310e9319a6b1f827
dhkey <dhkey14@icloud.com>
2024-10-11 10:53:58 +0300

    fixed 4th task

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -7,13 +7,13 @@ int main()
     int sallary = 0;
     cout << "Enter your sallary: "; cin >> sallary;
 
-    if  (sallary < 1000){
+    if  (sallary < 1000)
         cout << "You must to work harder!" << endl;
+
+    else if ( sallary > 999) {
+        if (sallary < 999999)
+            cout << "Great job!" << endl;
     }
-    else if (( sallary > 999) * (sallary < 999999)) { // alternative to AND operator, like if some of conditions is false, 1*0 = 0, smth like that
-        cout << "Great job!" << endl;
-    }
-    else if (sallary > 1000000) {
+    else if (sallary > 1000000)
         cout << "You are the millionaire! Congrats!" << endl;
-    }
 }
