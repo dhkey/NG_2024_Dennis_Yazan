82d2c4bfdf0c9b9090c73dded79092058d7bcd06
dhkey <dhkey14@icloud.com>
2024-10-16 21:09:37 +0300

    added Lesson_2/Task_1

--- Lesson_2/Task_1/main.cpp
+++ Lesson_2/Task_1/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int arr[5];
-    for (int count = 0; count < 5; count ++) { cin >> arr[count]; } // inserting numbser into the array
+    for (int count = 0; count < 5; count ++) { cout << "Enter " << count << "st number: ";  cin >> arr[count]; } // inserting numbser into the array
     for (int count = 0; count < 5; count ++) {
         count != 4 ? cout << arr[count] << ", " : cout << arr[count] << endl; // outputing using ternar operator, like if cout not equals to 4 - display without ','
     }
