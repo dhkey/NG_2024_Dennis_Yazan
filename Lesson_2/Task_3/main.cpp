1a8b81279dc4b3808fdd5252d8deaa6aa98aa83e
dhkey <dhkey14@icloud.com>
2024-10-18 23:25:59 +0300

    added fixed Lesson_2/Task_3

--- Lesson_2/Task_3/main.cpp
+++ Lesson_2/Task_3/main.cpp
@@ -13,14 +13,13 @@ int main()
         int value; cout << "Enter " << count+1 << "th value: "; cin >> value; //inserting inout into value
         if (value > arrMaxValue) arrMaxValue = value; //saving biggest value to calculate spaces
         if (value == 0){
-            count < 20 ? arr[count] = 0 : true; break; //breaking the input loop while input == 0
+            arr[count] = 0; break; //breaking the input loop while input == 0
         }else{ arr[count] = value; }
     } cout<<endl;
 
     int count = 0;
 
-    while (arr[count] !=0){
-
+    while (arr[count] !=0 && count < 20){
         for (int spacesCount = 0; spacesCount < ceil((arrMaxValue-arr[count])/2); spacesCount++) cout << " "; //printing spaces
         for (int starsCount = 0; starsCount < arr[count]; starsCount++) cout << "*"; //printing stars
         cout << endl;
