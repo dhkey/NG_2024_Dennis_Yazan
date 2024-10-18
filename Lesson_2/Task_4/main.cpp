a9df793d7ba21fc3bac965440b479cc0dd2082fe
dhkey <dhkey14@icloud.com>
2024-10-18 22:51:01 +0300

    added Lesson_2/Task_4

---
+++ Lesson_2/Task_4/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+//Dont work for the cyrylic, appears that it looks like 2 chars for compiler. Do we need to take care of it?
+
+int main()
+{
+    char line[64] = ""; cout << "Enter line(under 64 chars): "; cin.getline(line, 64); //initialization + filling
+
+    int count = 0;
+    while ( line[count] != 0) count++; //counting chars
+
+    cout<<count<<endl;
+
+}
