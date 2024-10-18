085a670aa1311acfe95a79d18052a9b644fd43ef
dhkey <dhkey14@icloud.com>
2024-10-18 22:54:10 +0300

    added Lesson_2/Task_4

--- Lesson_2/Task_4/main.cpp
+++ Lesson_2/Task_4/main.cpp
@@ -9,7 +9,7 @@ int main()
     char line[64] = ""; cout << "Enter line(under 64 chars): "; cin.getline(line, 64); //initialization + filling
 
     int count = 0;
-    while ( line[count] != 0) count++; //counting chars
+    while ( line[count]) count++; //counting chars
 
     cout<<count<<endl;
 
