a55d360f1bc059a030d5f24658551798389bf0c6
dhkey <dhkey14@icloud.com>
2024-10-22 01:24:54 +0300

    fixed Lesson_2/Task_2

--- Lesson_2/Task_2/main.cpp
+++ Lesson_2/Task_2/main.cpp
@@ -6,35 +6,28 @@ int main()
 {
     int arr[5];
 
-    for (int count = 0; count < 5; count ++) { cout << "Enter " << count << "st number: ";  cin >> arr[count]; } // inserting numbser into the array
+    for (int count = 0; count < 5; count ++) {
+        cout << "Enter " << count + 1 << "st number: ";
+        cin >> arr[count];
+    }
 
     bool isNotZeroLike = true;
     while (isNotZeroLike){
 
+        isNotZeroLike = false;
+
         for (int isZeroCount=0; isZeroCount < 5; isZeroCount++){
-            // arr[isZeroCount] != 0 ? cout<<"* " : cout << "";
-            // arr[isZeroCount] != 0 ? arr[isZeroCount]-=1 : true; //reducing by one each iteration
 
             if (arr[isZeroCount] != 0){
                 cout<<"*";
-                // cout << arr[isZeroCount];
                 arr[isZeroCount]-=1;
+                isNotZeroLike = true;
             }
             else{
                 cout << " ";
             }
         }
 
-        isNotZeroLike = false;
-        for (int isZeroCount = 0; isZeroCount < 5; isZeroCount++) {
-            if (arr[isZeroCount] != 0) {
-                isNotZeroLike = true;
-                break;
-            }
-        }
-
-
         cout << endl;
     }
 }
-
