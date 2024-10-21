b6b08d4c558999612ddf07ec638350fc0ac48c38
dhkey <dhkey14@icloud.com>
2024-10-22 01:15:55 +0300

    fixed Lesson_2/Task_4

--- Lesson_2/Task_4/main.cpp
+++ Lesson_2/Task_4/main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 
-using namespace std;
 
-//Dont work for the cyrylic, appears that it looks like 2 chars for compiler. Do we need to take care of it?
+using namespace std;
 
 int main()
 {
-    char line[64] = ""; cout << "Enter line(under 64 chars): "; cin.getline(line, 64); //initialization + filling
 
-    int count = 0;
-    while ( line[count]) count++; //counting chars
+    int ImprovizedDict[188] = {0};
+    for (int count = 32, countNum = 0; count <= 126 ; count++, countNum+=2)
+        ImprovizedDict[countNum] = count;
 
-    cout<<count<<endl;
+    char line[64] = "";
+    cout << "Enter line(under 64 chars): ";
+    cin.getline(line, 64);
+
+
+    for (int count = 0; count < 64 && line[count]; count ++)
+        ImprovizedDict[((int(line[count])-32)*2)+1]++;
+
+    for (int i=0; i<188; i+=2)
+        cout << char(ImprovizedDict[i]) << " ::: " << ImprovizedDict[i+1] << endl;
+
+
 
 }
