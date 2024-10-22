c1d2672a0ebde10607d666a6d43834156251d29f
dhkey <dhkey14@icloud.com>
2024-10-22 10:27:29 +0300

    fixed Lesson_2/Task_4

--- Lesson_2/Task_4/main.cpp
+++ Lesson_2/Task_4/main.cpp
@@ -6,14 +6,13 @@ using namespace std;
 int main()
 {
 
-    int ImprovizedDict[188] = {0};
+    int ImprovizedDict[189] = {0};
     for (int count = 32, countNum = 0; count <= 126 ; count++, countNum+=2)
         ImprovizedDict[countNum] = count;
 
     char line[64] = "";
     cout << "Enter line(under 64 chars): ";
     cin.getline(line, 64);
-
 
     for (int count = 0; count < 64 && line[count]; count ++)
         ImprovizedDict[((int(line[count])-32)*2)+1]++;
